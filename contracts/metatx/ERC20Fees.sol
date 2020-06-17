54736612a50fe8e5ecf48f0650321f6946cf6f98
Nathan Sala <sala.nathan@gmail.com>
2020-06-17 23:36:45 +0800

    fix solidity version in contracts

--- contracts/metatx/ERC20Fees.sol
+++ contracts/metatx/ERC20Fees.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.6.8;
+pragma solidity 0.6.8;
 
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/GSN/GSNRecipient.sol";
